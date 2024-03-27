caa7c2e3cbce408d6cd5cba005413e0b9e5eca78
yeonii01 <112497688+yeonii01@users.noreply.github.com>
2024-03-27 13:33:57 +0900

    24 3 27 Particle 속도 부여
    Particle 속도 부여 및 ParticleCloudShader 분리

--- SimpleGame/Renderer.cpp
+++ SimpleGame/Renderer.cpp
@@ -6,7 +6,6 @@ Renderer::Renderer(int windowSizeX, int windowSizeY)
 	Initialize(windowSizeX, windowSizeY);
 }
 
-
 Renderer::~Renderer()
 {
 }
@@ -20,6 +19,7 @@ void Renderer::Initialize(int windowSizeX, int windowSizeY)
 	//Load shaders
 	m_SolidRectShader = CompileShaders("./Shaders/SolidRect.vs", "./Shaders/SolidRect.fs");
 	m_ParticleShader = CompileShaders("./Shaders/Particle.vs", "./Shaders/Particle.fs");
+	m_ParticleCloudShader = CompileShaders("./Shaders/ParticleCloud.vs", "./Shaders/ParticleCloud.fs");
 
 	//Create VBOs
 	CreateVertexBufferObjects();
@@ -224,35 +224,80 @@ void Renderer::CreateParticleCloud(int numParticles)
 	float size = 0.01f;
 	int particleCount = numParticles;
 	int vertexCount = particleCount * 6;
-	int floatCount = vertexCount * 3;
+	int floatCount = vertexCount * (3 + 1 + 3 + 1); //x, y, z, starttime, vx, vy, vz
 
 	float* vertices = NULL;
 	vertices = new float[floatCount];
 
+	float vx, vy, vz;
+	float lifeTime;
+
 	int index = 0;
 	for (int i = 0; i < particleCount; ++i)
 	{
+		float startTime = 6.f*((float)rand() / (float)RAND_MAX);
 		centerX = ((float)rand() / (float)RAND_MAX) * 2.f - 1.f;
 		centerY = ((float)rand() / (float)RAND_MAX) * 2.f - 1.f;
-		vertices[index] = centerX - size; index++;
-		vertices[index] = centerY - size; index++;
-		vertices[index] = 0.f; index++;
-		vertices[index] = centerX + size; index++;
-		vertices[index] = centerY + size; index++;
-		vertices[index] = 0.f; index++;
-		vertices[index] = centerX - size; index++;
-		vertices[index] = centerY + size; index++;
-		vertices[index] = 0.f; index++; //trianlge1
+		vx = ((float)rand() / (float)RAND_MAX * 2.f - 1.f);
+		vy = ((float)rand() / (float)RAND_MAX * 2.f - 1.f);
+		vz = ((float)rand() / (float)RAND_MAX * 2.f - 1.f);
+		lifeTime = 4.f * ((float)rand() / (float)RAND_MAX) + 1.f;
 
 		vertices[index] = centerX - size; index++;
 		vertices[index] = centerY - size; index++;
 		vertices[index] = 0.f; index++;
+		vertices[index] = startTime; index++;
+		vertices[index] = vx; index++;
+		vertices[index] = vy; index++;
+		vertices[index] = vz; index++;
+		vertices[index] = lifeTime; index++;
+		
 		vertices[index] = centerX + size; index++;
 		vertices[index] = centerY + size; index++;
 		vertices[index] = 0.f; index++;
+		vertices[index] = startTime; index++;
+		vertices[index] = vx; index++;
+		vertices[index] = vy; index++;
+		vertices[index] = vz; index++;
+		vertices[index] = lifeTime; index++;
+
+		vertices[index] = centerX - size; index++;
+		vertices[index] = centerY + size; index++;
+		vertices[index] = 0.f; index++;
+		vertices[index] = startTime; index++;	
+		vertices[index] = vx; index++;
+		vertices[index] = vy; index++;
+		vertices[index] = vz; index++;
+		vertices[index] = lifeTime; index++;
+
+		//trianlge1
+
+		vertices[index] = centerX - size; index++;
+		vertices[index] = centerY - size; index++;
+		vertices[index] = 0.f; index++;
+		vertices[index] = startTime; index++;
+		vertices[index] = vx; index++;
+		vertices[index] = vy; index++;
+		vertices[index] = vz; index++;
+		vertices[index] = lifeTime; index++;
+
+		vertices[index] = centerX + size; index++;
+		vertices[index] = centerY + size; index++;
+		vertices[index] = 0.f; index++;
+		vertices[index] = startTime; index++;
+		vertices[index] = vx; index++;
+		vertices[index] = vy; index++;
+		vertices[index] = vz; index++;
+		vertices[index] = lifeTime; index++;
+
 		vertices[index] = centerX + size; index++;
 		vertices[index] = centerY - size; index++;
-		vertices[index] = 0.f; index++;	//trianlge2
+		vertices[index] = 0.f; index++;	
+		vertices[index] = startTime; index++;		
+		vertices[index] = vx; index++;
+		vertices[index] = vy; index++;
+		vertices[index] = vz; index++;		
+		vertices[index] = lifeTime; index++;	//trianlge2
 	}
 	glGenBuffers(1, &m_ParticleCloudVBO);
 	glBindBuffer(GL_ARRAY_BUFFER, m_ParticleCloudVBO);
@@ -303,7 +348,7 @@ void Renderer::DrawParticle()
 void Renderer::DrawParticleCloud()
 {
 	//Program select
-	GLuint shader = m_ParticleShader;
+	GLuint shader = m_ParticleCloudShader;
 	glUseProgram(shader);
 
 	int uTime = glGetUniformLocation(shader, "u_Time");
@@ -317,7 +362,34 @@ void Renderer::DrawParticleCloud()
 	int attribPosition = glGetAttribLocation(shader, "a_Position");
 	glEnableVertexAttribArray(attribPosition);
 	glBindBuffer(GL_ARRAY_BUFFER, m_ParticleCloudVBO);
-	glVertexAttribPointer(attribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+	glVertexAttribPointer(attribPosition, 
+							3, GL_FLOAT, 
+							GL_FALSE, 
+							sizeof(float) * 8, 0);
+
+	int attribStartTime = glGetAttribLocation(shader, "a_StartTime");
+	glEnableVertexAttribArray(attribStartTime);
+	glBindBuffer(GL_ARRAY_BUFFER, m_ParticleCloudVBO);
+	glVertexAttribPointer(attribStartTime,
+							1, GL_FLOAT,
+							GL_FALSE,
+							sizeof(float) * 8, (GLvoid*)(sizeof(float) * 3));
+
+	int attribVelocity = glGetAttribLocation(shader, "a_Velocity");
+	glEnableVertexAttribArray(attribVelocity);
+	glBindBuffer(GL_ARRAY_BUFFER, m_ParticleCloudVBO);
+	glVertexAttribPointer(attribVelocity,
+		3, GL_FLOAT,
+		GL_FALSE,
+		sizeof(float) * 8, (GLvoid*)(sizeof(float) * 4));
+
+	int attribLifeTime = glGetAttribLocation(shader, "a_LifeTime");
+	glEnableVertexAttribArray(attribLifeTime);
+	glBindBuffer(GL_ARRAY_BUFFER, m_ParticleCloudVBO);
+	glVertexAttribPointer(attribLifeTime,
+		1, GL_FLOAT,
+		GL_FALSE,
+		sizeof(float) * 8, (GLvoid*)(sizeof(float) * 5));
 
 	glDrawArrays(GL_TRIANGLES, 0, m_ParticleCloudVertexCount);	//모드 선택
 	//이 함수 호출 즉시 GPU가 동작함
