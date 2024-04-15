af4c98e1dbb13905c68069506ce4ff15483bcdab
yeonii01 <112497688+yeonii01@users.noreply.github.com>
2024-04-16 00:35:10 +0900

    24 4 16 FS를 이용한 변화 만들기
    움직이는 것처럼(실제로 움직임X)로 보이도록 FS 작성

--- SimpleGame/Shaders/FSSandbox.fs
+++ SimpleGame/Shaders/FSSandbox.fs
@@ -1,9 +1,13 @@
 #version 330
 
 layout(location=0) out vec4 FragColor;
+
+uniform float u_Time;
 in vec4 v_Color;
 
-void main()
+const float c_PI = 3.141592;
+
+void FilledCircle()
 {
 	vec4 newColor = vec4(1,1,1,1);
 	float r = 0.5;
@@ -18,4 +22,39 @@ void main()
 		newColor = vec4(0,0,0,0);
 	}
 	FragColor = newColor;
+}
+
+void Circle()
+{
+	vec4 newColor = vec4(1,1,1,1);
+	float r = 0.5;
+	float width = 0.05;
+
+	vec2 center = vec2(0.5, 0.5);
+	float dist = distance(v_Color.rg, center);
+	if(dist > r - width && dist < r)
+	{
+		newColor = vec4(1,1,1,1);
+	}
+	else
+	{
+		newColor = vec4(0,0,0,0);
+	}
+	FragColor = newColor;
+}
+
+void Circles()
+{
+	float circleCount = 10;
+	vec2 circleCenter = vec2(0.5, 0.5);
+	float dist = distance(v_Color.rg, circleCenter);
+	float Input = circleCount*c_PI*4 * dist - u_Time * 10.0;
+	float sinValue = pow(sin(Input),16);
+	FragColor = vec4(sinValue);
+}
+
+void main()
+{
+	 //Circle();
+	 Circles();
 }
